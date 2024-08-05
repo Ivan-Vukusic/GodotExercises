c56c833ea6e92ea5f78232e0ae19d4d7fd96ada0
Ivan Vukušić <ivukusic27@gmail.com>
2024-08-05 20:54:35 +0200

    Dodao kontrole igrača

--- LeteciTanjur/player.gd
+++ LeteciTanjur/player.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	apply_impulse(Vector2(25, 0))
-
-
-
+func _physics_process(delta):
+	var potisak = 700
+	if Input.is_action_pressed("kretanje_desno"):
+		apply_force(Vector2(potisak, 0))
+	if Input.is_action_pressed("kretanje_lijevo"):
+		apply_force(Vector2(-potisak, 0))
+	if Input.is_action_pressed("kretanje_gore"):
+		apply_force(Vector2(0, -potisak))
+	if Input.is_action_pressed("kretanje_dolje"):
+		apply_force(Vector2(0, potisak))
