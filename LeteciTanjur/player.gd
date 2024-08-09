c3e55250ddb4b0e4ca1cd552025e273ab821b2af
Ivan Vukušić <ivukusic27@gmail.com>
2024-08-09 19:46:41 +0200

    Dodao signal za reset i otežao kontrole

--- LeteciTanjur/player.gd
+++ LeteciTanjur/player.gd
@@ -1,8 +1,8 @@
 extends RigidBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _physics_process(delta):
-	var potisak = 700
+var potisak = 1000
+
+func _physics_process(delta):	
 	if Input.is_action_pressed("kretanje_desno"):
 		apply_force(Vector2(potisak, 0))
 	if Input.is_action_pressed("kretanje_lijevo"):
